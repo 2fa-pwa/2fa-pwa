1d130e3ee70155468016f41d98f8ba1a16dca075
Simon <62551180+simonagleev@users.noreply.github.com>
2022-03-17 13:17:10 +0300

    last-changes

--- src/lib/services/base/CapturerService.ts
+++ src/lib/services/base/CapturerService.ts
@@ -11,6 +11,8 @@ export class CapturerService {
     videoService = inject<VideoService>(TYPES.videoService);
     routerService = inject<RouterService>(TYPES.routerService);
 
+
+    
     constructor() {
         let interval: NodeJS.Timeout;
         autorun(() => {
@@ -29,10 +31,38 @@ export class CapturerService {
         }); 
     };
 
-    processBlob = () => {
+    processBlob = async () => {
         console.log('кадр')
+        const { mediaStream } = this.videoService;
+        
+        if(mediaStream) {
+            const track = mediaStream.getVideoTracks()[0];
+            const capturer = new ImageCapture(track);
+            const frame: ImageBitmap = await capturer.grabFrame();
+            
+            console.log('capturer ')
+            console.log(frame)
+            
+            const canvas = document.createElement("canvas");
+            canvas.width = 500;
+            canvas.height = 500;
+            const ctx = canvas.getContext("2d");
+            
+            if (ctx) {
+                ctx.drawImage(frame, 0, 0, 500, 500);
+                canvas.toBlob(async (blob) => {
+                  // Blob передается на сканирование Qr кода
+                  frame.close();
+                }, 'image/jpeg');
+            }
+            const twofactor = import("node-2fa");
+            // const newSecret = twofactor.generateSecret({ name: "My Awesome App", account: "johndoe" });
+            // const newToken = twofactor.generateToken("XDQXYCP5AC6FA32FQXDGJSPBIDYNKK5W");
+            // twofactor.verifyToken("XDQXYCP5AC6FA32FQXDGJSPBIDYNKK5W", "630618");
+        }
     };
     
 };
 
 export default CapturerService;
+   
