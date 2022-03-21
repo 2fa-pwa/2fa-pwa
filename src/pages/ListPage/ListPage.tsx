1cb46f7929ccd5b394b7568bd2d2c0de55073f91
Simon <62551180+simonagleev@users.noreply.github.com>
2022-03-21 12:27:19 +0300

    breadcrumbs

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
+import { Breadcrumbs, IOption } from 'react-declarative';
 import { ListSubheader, Paper } from '@mui/material';
 
+import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from './components/ListItem';
 import MatListItem from '@mui/material/ListItem';
@@ -9,40 +11,57 @@ import MatListItemText from '@mui/material/ListItemText';
 import ioc from '../../lib/ioc';
 import { observer } from "mobx-react-lite";
 
+const actions: IOption[] = [
+  {
+    action: 'export-action',
+    label: 'Export tokens'
+  },
+  {
+    action: 'import-action',
+    label: 'Import tokens'
+  }
+];
+
 export const ListPage = () => {
 
   const { authList } = ioc.listService;
 
   return (
-    <Paper sx={{
-      height: 'calc(100vh - 80px)',
-      
-    }}>
-      <List
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            QR list
-          </ListSubheader>
-        }
-        sx={{
-          '& > .MuiListItem-root:nth-child(2n + 1)': {
-            background: '#0001',
+    <Box>
+      <Breadcrumbs
+        disabled
+        actions={actions}
+      />
+      <Paper sx={{
+        height: 'calc(100vh - 145px)',
+
+      }}>
+        <List
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              QR list
+            </ListSubheader>
           }
-        }}
-      >
-        {authList.map(([id, item], idx) => (
-          <ListItem key={idx} authItem={item} authId={id} />
-        ))}
-        {authList.length === 0 && (
-          <MatListItem>
-            <MatListItemText
-              primary="Nothing found"
-              secondary="You should scan a new token on Scanner page"
-            />
-          </MatListItem>
-        )}
-      </List>
-    </Paper >
+          sx={{
+            '& > .MuiListItem-root:nth-child(2n + 1)': {
+              background: '#0001',
+            }
+          }}
+        >
+          {authList.map(([id, item], idx) => (
+            <ListItem key={idx} authItem={item} authId={id} />
+          ))}
+          {authList.length === 0 && (
+            <MatListItem>
+              <MatListItemText
+                primary="Nothing found"
+                secondary="You should scan a new token on Scanner page"
+              />
+            </MatListItem>
+          )}
+        </List>
+      </Paper >
+    </Box>
   );
 };
 
